ca88eac1d9a18760252926ce8bbfcd4a1579e170
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-31 22:13:27 +0900

    update RGBスライダースタイル作成

--- src/ColorPickerPage.tsx
+++ src/ColorPickerPage.tsx
@@ -1,12 +1,53 @@
 import Div100vh from "react-div-100vh";
-import { ColorPicker } from "@mantine/core";
+import { ColorPicker, Slider, NumberInput } from "@mantine/core";
 
 import "./styles/colorPickerPage.css";
+import { useState } from "react";
 
 export const ColorPickerPage = () => {
+  const [redValue, setRedValue] = useState(255);
+  const [greenValue, setGreenValue] = useState(255);
+  const [blueValue, setBlueValue] = useState(255);
+
   return (
     <Div100vh>
       <ColorPicker style={{ width: "fit-content", margin: "0 auto" }} />
+      <section className="rgbSectionStyle">
+        <div className="sliderWrap redSlider">
+          <p className="sliderLabel">R</p>
+          <Slider
+            max={255}
+            value={redValue}
+            onChange={setRedValue}
+            style={sliderStyle}
+          />
+          <NumberInput hideControls value={redValue} />
+        </div>
+        <div className="sliderWrap greenSlider">
+          <p className="sliderLabel">G</p>
+          <Slider
+            max={255}
+            value={greenValue}
+            onChange={setGreenValue}
+            style={sliderStyle}
+          />
+          <NumberInput hideControls value={greenValue} />
+        </div>
+        <div className="sliderWrap blueSlider">
+          <p className="sliderLabel">B</p>
+          <Slider
+            max={255}
+            value={blueValue}
+            onChange={setBlueValue}
+            style={sliderStyle}
+          />
+          <NumberInput hideControls value={blueValue} />
+        </div>
+      </section>
     </Div100vh>
   );
+};
+
+const sliderStyle = {
+  width: "210px",
 };
