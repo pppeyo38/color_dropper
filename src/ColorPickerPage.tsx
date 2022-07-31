d6a021d929027370d7c0487a75696954fb9a341c
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-31 23:43:16 +0900

    update rgbスライダーから値を取得

--- src/ColorPickerPage.tsx
+++ src/ColorPickerPage.tsx
@@ -2,16 +2,40 @@ import Div100vh from "react-div-100vh";
 import { ColorPicker, Slider, NumberInput } from "@mantine/core";
 
 import "./styles/colorPickerPage.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+type RGB = {
+  red: number;
+  green: number;
+  blue: number;
+};
 
 export const ColorPickerPage = () => {
   const [redValue, setRedValue] = useState(255);
   const [greenValue, setGreenValue] = useState(255);
   const [blueValue, setBlueValue] = useState(255);
 
+  const [value, onChange] = useState(
+    `rgba(${redValue}, ${greenValue}, ${blueValue}, 1)`
+  );
+
+  useEffect(() => {
+    onChange(`rgba(${redValue}, ${greenValue}, ${blueValue}, 1)`);
+  }, [redValue, greenValue, blueValue]);
+
+  const changeColor = () => {
+    onChange(`rgba(${redValue}, ${greenValue}, ${blueValue}, 1)`);
+  };
+
   return (
     <Div100vh>
-      <ColorPicker style={{ width: "fit-content", margin: "0 auto" }} />
+      <h2>{value}</h2>
+      <ColorPicker
+        format="rgb"
+        value={value}
+        onChange={changeColor}
+        style={{ width: "fit-content", margin: "0 auto" }}
+      />
       <section className="rgbSectionStyle">
         <div className="sliderWrap redSlider">
           <p className="sliderLabel">R</p>
