8b8c0605ba6b3b92ea44c44eba4ab0630398daf3
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-31 18:39:07 +0900

    update Organize packages

--- src/ColorPicker.tsx
+++ src/ColorPicker.tsx
@@ -1,10 +1,3 @@
-import { ChromePicker } from "react-color";
-import styles from "./styles/ColorPicker.module.scss";
-
 export const ColorPicker = () => {
-  return (
-    <div className={styles.text}>
-      <ChromePicker></ChromePicker>
-    </div>
-  );
+  return <div></div>;
 };
